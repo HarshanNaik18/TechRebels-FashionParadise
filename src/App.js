b08708c40ca2a6bb5f064c4b652e93209b849912
HarshanNaik18 <harshannaik18@gmail.com>
2023-05-26 20:18:05 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <Router>
-          <Routes>
-            <Route path="/" exact element={<Home/>} />
-            
+    <div className='AppBody'>
+      <Router>
+        <Navbar />
+        <div className='App'>
+          <Routes >
+            <Route path="/" exact element={<Home />} />
+
           </Routes>
+        </div>
       </Router>
     </div>
   );
