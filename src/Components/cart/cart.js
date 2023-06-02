e898dd53ba81e63bc490a3b1fa7f9fb821fe7d42
pavan-2562003 <pavankumar031@ooutlook.com>
2023-06-02 15:53:23 +0530

    added a file

--- src/Components/cart/cart.js
+++ src/Components/cart/cart.js
@@ -1,54 +1,92 @@
 import React, { useEffect, useState } from 'react';
-import { cartProducts } from './data';
-
-import Basket from './basket';
-import styled from 'styled-components';
-import './basket.css'
 import { collection, onSnapshot, query, where } from 'firebase/firestore';
 import { auth, db } from '../../Firebase/Firebase';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import Basket from './basket';
 
-
+const MySwal = withReactContent(Swal);
 
 const Cart = () => {
   const [cartData, setCartData] = useState([]);
 
-  const Cartbg = styled.div`
-  background-color:${cartData.length > 0 ?'lightgrey':'white'};`
+  const handleSuccess = () => {
+    MySwal.fire({
+      icon: 'success',
+      title: 'Payment was successful',
+      timer: 4000,
+    });
+  };
+
+  const handleFailure = () => {
+    MySwal.fire({
+      icon: 'error',
+      title: 'Payment was not successful',
+      timer: 4000,
+    });
+  };
+
+  const payNow = async (token) => {
+    try {
+      const response = await axios({
+        url: 'YOUR_SERVER_ENDPOINT', // Replace with your server endpoint
+        method: 'post',
+        data: {
+          amount: cartData.reduce((total, item) => total + item.price, 0) * 100,
+          token,
+        },
+      });
+      if (response.status === 200) {
+        handleSuccess();
+      }
+    } catch (error) {
+      handleFailure();
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    const collectionRef = collection(db, "cart");
+    const collectionRef = collection(db, 'cart');
     const userid = auth.currentUser.uid;
-    const q = query(collectionRef, where("user", "==", userid));
-    const Fetchdata = onSnapshot(q, querySnapshot => {
+    const q = query(collectionRef, where('user', '==', userid));
+    const Fetchdata = onSnapshot(q, (querySnapshot) => {
       const data = [];
-      querySnapshot.forEach(item => {
+      querySnapshot.forEach((item) => {
         data.push({ ...item.data(), id: item.id });
       });
       setCartData(data);
       console.log(data);
-      // setDisplayProjects(data);
     });
     return () => {
       Fetchdata();
-    }
-    //eslint-disable-next-line
+    };
   }, []);
+
   return (
-    <Cartbg>
+    <>
       <div className="title">
         <p>Your shopping basket</p>
       </div>
-      {
-        cartData.length>0?
-        cartData && cartData.map((item, index) => (
-          <Basket product={item} key={index} />
-        ))
-        :
-        <h1 style={{width:'100%',textAlign:'center',background:'white'}} >Cart is Empty</h1>
-      }
-    </Cartbg>
-  )
-
-}
+      {cartData.length > 0 ? (
+        cartData.map((item, index) => <Basket product={item} key={index} />)
+      ) : (
+        <h1 style={{ width: '100%', textAlign: 'center', background: 'white' }}>Cart is Empty</h1>
+      )}
+      <StripeCheckout
+        token={payNow}
+        stripeKey="pk_test_51Mb3okSIOkM9Vi78rdyJfqgdjOZdpQeuGnrb01ouB5AqlVAXfHejRAGJPSuvLy4Om92d03jrkuVPnHpLdDz0pZqI00RZaw8hKe" // Replace with your Stripe publishable key
+        name="Fashion Paradise" // Replace with your website name
+        amount={cartData.reduce((total, item) => total + item.price, 0) * 100}
+        currency="INR"
+        billingAddress={false}
+        shippingAddress={false}
+      >
+        <button className="pay-button">Pay Now</button>
+      </StripeCheckout>
+    </>
+  );
+};
 
 export default Cart;
-
